fba0ecd33e0640365eccb103b152c8428bf98cd5
ulziibat.n <ulziibat.n@gmail.com>
2024-03-12 21:38:06 +0800

    build

--- src/main.js
+++ src/main.js
@@ -6,10 +6,9 @@ document.body.classList.add('is-loading')
 console.log('Welcome to Vite + JS + Webflow!')
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.classList.remove('is-loading')
-  document.body.classList.add('is-loaded')
-
   Splitting()
   viewportDetect()
   hero()
+  document.body.classList.remove('is-loading')
+  document.body.classList.add('is-loaded')
 })
