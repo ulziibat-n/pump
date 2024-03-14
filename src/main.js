37a62ea797f2b26c2559535d4a887e5e87c76f6d
ulziibat.n <ulziibat.n@gmail.com>
2024-03-14 17:30:55 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
 import Lenis from '@studio-freight/lenis'
 import gsap from 'gsap'
-import { CustomEase, ScrollTrigger } from 'gsap/all'
-import Splitting from 'splitting'
+import { CustomEase, ScrollTrigger, SplitText } from 'gsap/all'
 import Swiper from 'swiper'
 import { Pagination } from 'swiper/modules'
 import 'swiper/css'
-import 'splitting/dist/splitting.css'
 import './styles/style.scss'
 
-gsap.registerPlugin(CustomEase, ScrollTrigger)
+gsap.registerPlugin(CustomEase, ScrollTrigger, SplitText)
 
 CustomEase.create('base', '0.075, 0.82, 0.165, 1')
 
 document.body.classList.add('is-loading')
-console.log('Welcome to Vite + JS + Webflow!')
+console.log('Welcome to Vite + JS + Webflow! 😍')
 
-document.addEventListener('DOMContentLoaded', () => {
+window.onload = () => {
   const lenis = new Lenis()
-  lenis.on('scroll', (e) => {
-    console.log(e)
-  })
+  // lenis.on('scroll', (e) => {
+  //   console.log(e)
+  // })
 
   lenis.on('scroll', ScrollTrigger.update)
 
@@ -28,10 +26,14 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   gsap.ticker.lagSmoothing(0)
+
+  console.log('Lenis ✅')
   document.body.classList.remove('is-loading')
   document.body.classList.add('is-loaded')
+  console.log('Site Loaded ✅')
   runSections()
-})
+  console.log('Hero Section ✅')
+}
 
 function runSections() {
   // Get the element from the DOM
@@ -39,28 +41,74 @@ function runSections() {
 
   // If it exists, play the aniamtion
   if (sectionHero) {
-    const heroElements = sectionHero.querySelectorAll('.hero-content > *')
-    const heroTitle = Splitting({
-      target: sectionHero.querySelector('h1'),
-      by: 'lines',
-    })
-
+    const heroTitle = new SplitText('.hero-content h1', { type: 'lines' })
+    let heroElements = gsap.utils.toArray([
+      '.hero-content p',
+      '.hero-content a',
+    ])
     if (heroElements && heroTitle) {
-      gsap.fromTo(
-        heroElements,
-        {
-          opacity: 0,
-          y: '3rem',
-        },
-        {
-          opacity: 1,
-          y: '0rem',
-          duration: 2,
+      const heroTimeline = gsap.timeline({
+        defaults: {
+          // children inherit these defaults
+          duration: 1,
           ease: 'base',
-          stagger: 0.1,
-          delay: 0.1,
-        }
-      )
+        },
+      })
+
+      console.log(heroTitle)
+      console.log(heroElements)
+
+      heroElements = gsap.utils.toArray(heroElements)
+      heroElements.unshift(heroTitle.lines)
+
+      heroTimeline
+        .fromTo(
+          heroElements,
+          {
+            opacity: 0,
+            y: '3rem',
+          },
+          {
+            opacity: 1,
+            y: '0rem',
+            duration: 2,
+            stagger: 0.1,
+            delay: 0.1,
+          }
+        )
+        .fromTo(
+          '.hero-width.is-main',
+          {
+            opacity: 0,
+            y: '1rem',
+            scale: 0.95,
+          },
+          {
+            opacity: 1,
+            y: '0rem',
+            scale: 1,
+            delay: 0,
+            duration: 2,
+          },
+          '-=1'
+        )
+        .fromTo(
+          '.hero-width.is-grow .hero-slider-item',
+          {
+            opacity: 0,
+            y: '3rem',
+          },
+          {
+            opacity: 1,
+            y: '0rem',
+            duration: 2,
+            stagger: {
+              each: 0.1,
+              from: 'center',
+            },
+          },
+          '-=1.5'
+        )
     }
 
     // Get the element from the DOM
